8a867e65da1bcf16b2d2e30c0477783fdf5233bd
YassineElGhandour <yassine.elghandour25@gmail.com>
2025-07-16 22:37:03 +0300

    (test): init virtualaquariummanager unit tests project

---
+++ Controllers/TankControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using VirtualAquariumManager.Controllers;
+using VirtualAquariumManager.Models;
+using VirtualAquariumManager.ViewModels;
+
+namespace VirtualAquariumManager.Tests.Controllers
+{
+    public class TankControllerTests
+    {
+        [Fact]
+        public void Test_Index_GetAllTanksSuccessfully()
+        {
+            
+        }
+    }
+}
